6a7e3c682d5d0583a440f10cf0cf450c45814e84
Polina <112728064+laylawinxp@users.noreply.github.com>
2023-11-12 22:18:21 +0300

    Лабораторная работа 4

---
+++ Лабораторная работа 4/task4.1.py
@@ -0,0 +1,13 @@
+import json
+
+INPUT_FILE = "input.json"
+
+
+def task() -> float:
+    with open(INPUT_FILE) as file:
+        data = json.load(file)
+    sum_ = sum(i["score"] * i["weight"] for i in data)
+    return round(sum_, 3)
+
+
+print(task())
